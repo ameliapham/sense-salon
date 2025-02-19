7a897bd56ed3fbed4bafdd37add81d4238240c8f
Huong Pham <59421719+ameliapham@users.noreply.github.com>
2025-02-20 00:54:54 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center justify-center bg-blue-500 p-4 text-white">
+    Hello, Nuxt + Tailwind!
+  </div>
+</template>
+
+<style>
+@import "~/assets/css/tailwind.css";
+</style>
