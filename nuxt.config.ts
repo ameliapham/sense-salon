281b4156575103b18b55b2a978d41b18a8b1f153
ameliapham <ptt.huong.8895@gmail.com>
2025-02-23 22:37:18 +0100

    Set up svg and add logo.svg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import svgLoader from 'vite-svg-loader'
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  vite: {
+    plugins: [svgLoader()],
+  },
 });
