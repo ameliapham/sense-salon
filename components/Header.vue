0d57a6a95f8777dd7741efc443d5c69a1defe039
ameliapham <ptt.huong.8895@gmail.com>
2025-02-24 03:06:46 +0100

    Update Header

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Button from "~/components/Button.vue";
+import Logo from "~/assets/svg/logo.svg";
+</script>
+
+<template>
+  <header class="bg-primary absolute z-30 w-full">
+    <div class="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
+      <NuxtLink to="/" class="flex items-center">
+        <Logo class="h-12 w-auto" />
+      </NuxtLink>
+
+      <nav class="flex items-center">
+        <ul class="flex flex-row items-center gap-8">
+          <li>
+            <NuxtLink to="/about" class="font-body text-textAccent text-sm uppercase border-b border-transparent hover:border-textAccent pb-1 transition delay-150 duration-300">About Us</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about" class="font-body text-textAccent text-sm uppercase border-b border-transparent hover:border-textAccent pb-1 transition delay-150 duration-300">Our Service</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about" class="font-body text-textAccent text-sm uppercase border-b border-transparent hover:border-textAccent pb-1 transition delay-150 duration-300">Our Works</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about" class="font-body text-textAccent text-sm uppercase border-b border-transparent hover:border-textAccent pb-1 transition delay-150 duration-300">Contact Us</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
