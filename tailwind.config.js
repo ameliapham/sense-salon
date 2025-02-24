b104c1c69f47f997123d5a556040e54cc367e6d0
ameliapham <ptt.huong.8895@gmail.com>
2025-02-24 03:06:22 +0100

    Format and add conditions for Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./components/**/*.{vue,js,ts}",
-    "./layouts/**/*.{vue,js,ts}",
-    "./pages/**/*.{vue,js,ts}",
-    "./plugins/**/*.{js,ts}",
-  ],
+  content: ["./components/**/*.{vue,js,ts}", "./layouts/**/*.{vue,js,ts}", "./pages/**/*.{vue,js,ts}", "./plugins/**/*.{js,ts}"],
   theme: {
     extend: {
       colors: {
@@ -13,15 +8,14 @@ export default {
         secondary: "#8C834D",
         accent: "#FFD85D",
         background: "#F0EADB",
-        text: {
-          primary: "#343B22",
-          secondary: "#2C323F",
-        },
+        textPrimary: "#343B22",
+        textSecondary: "#2C323F",
+        textAccent: "#F0EADB",
       },
       fontFamily: {
         heading: ["Playfair Display", "serif"],
         body: ["Montserrat", "sans-serif"],
-      }
+      },
     },
   },
   plugins: [],
