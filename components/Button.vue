5ee80e01471c012933d2f1646ea6c351ea1cb778
ameliapham <ptt.huong.8895@gmail.com>
2025-02-25 20:33:15 +0100

    Creating and styling Button

---
+++ components/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  to: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink :to="to" class="rounded-b-full rounded-r-full border border-secondary px-10 py-1 text-primary transition delay-75 duration-300 hover:bg-accent text-base">
+    {{ label }}
+  </NuxtLink>
+</template>
