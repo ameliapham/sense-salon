5b44d8a94830caafa4e9c64e476eac9296c8f420
ameliapham <ptt.huong.8895@gmail.com>
2025-02-27 17:20:19 +0100

    Update button width style

--- components/Button.vue
+++ components/Button.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="to" class="rounded-b-full rounded-r-full border border-secondary px-10 py-1 text-primary transition delay-75 duration-300 hover:bg-accent text-base">
+  <NuxtLink :to="to" class="w-fit rounded-b-full rounded-r-full border border-secondary px-10 py-1 text-base text-primary transition delay-75 duration-300 hover:bg-accent">
     {{ label }}
   </NuxtLink>
 </template>
