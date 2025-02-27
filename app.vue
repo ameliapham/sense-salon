18131cc6bf4d608f2e81eccebb9e168b14fc087b
ameliapham <ptt.huong.8895@gmail.com>
2025-02-27 17:22:07 +0100

    Add HeroSection in app

--- app.vue
+++ app.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import Header from "~/components/Header.vue";
 import Footer from "~/components/Footer.vue";
+import Button from "~/components/Button.vue";
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="flex min-h-screen flex-col bg-background">
     <Header />
-    <main class="flex flex-1 items-center justify-center font-heading text-2xl text-primary">Hello World</main>
+    <main class="flex flex-col">
+      <HeroSection />
+    </main>
     <Footer />
   </div>
 </template>
