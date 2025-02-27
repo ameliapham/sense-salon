70573af2769f8de2f67bff21a672af6db8e730f9
ameliapham <ptt.huong.8895@gmail.com>
2025-02-27 17:19:50 +0100

    Update style extension

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from "tailwindcss";
 
-
-const config: Config ={
-  content: [
-    "./components/**/*.{vue,js,ts}", 
-    "./layouts/**/*.{vue,js,ts}", "./pages/**/*.{vue,js,ts}", 
-    "./plugins/**/*.{js,ts}"
-  ],
+const config: Config = {
+  content: ["./components/**/*.{vue,js,ts}", "./layouts/**/*.{vue,js,ts}", "./pages/**/*.{vue,js,ts}", "./plugins/**/*.{js,ts}"],
   theme: {
     extend: {
       colors: {
@@ -19,27 +14,38 @@ const config: Config ={
         textAccent: "#F0EADB",
       },
       fontFamily: {
-        heading: ["Playfair Display", "serif"],
-        body: ["Montserrat", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+        sansSerif: ["Montserrat", "sans-serif"],
       },
     },
   },
   plugins: [
-    function ({ addBase } : { addBase: (styles: Record<string, any>) => void }) {
+    function ({ addBase }: { addBase: (styles: Record<string, any>) => void }) {
       addBase({
-        "h4, h5, h6, p, a, ul, ol, li": {
-          fontFamily: "Montserrat, sans-serif",
-        },
-        "h1, h2": {
-          fontFamily: "Playfair Display, serif",
-        },
-        "h3": {
-          fontFamily: "Montserrat, sans-serif",
+        heading: {
+          fontFamily: "theme('fontFamily.serif')",
+          fontSize: "theme('fontSize.9xl')",
           textTransform: "uppercase",
+        },
+        h1: {
+          fontFamily: "theme('fontFamily.serif')",
+          fontSize: "theme('fontSize.4xl')",
+        },
+        h3: {
+          fontFamily: "theme('fontFamily.sansSerif')",
+          fontSize: "theme('fontSize.2xl')",
+          textTransform: "uppercase",
+        },
+        p: {
+          fontFamily: "theme('fontFamily.sansSerif')",
+          fontSize: "theme('fontSize.base')",
+        },
+        "h5, h6, a, ul, ol, li": {
+          fontFamily: "theme('fontFamily.sansSerif')",
         },
       });
     },
   ],
 };
 
-export default config
+export default config;
