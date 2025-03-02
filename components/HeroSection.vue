53f5c91802f35c08dd1be0dc36daae1271d1eb66
ameliapham <ptt.huong.8895@gmail.com>
2025-03-02 23:30:27 +0100

    Update styles

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="grid max-h-[850px] w-full grid-cols-[5fr_3fr_1fr_3fr_5fr] grid-rows-[2fr_2fr_1fr_1fr_2fr]">
+  <div class="grid max-h-[850px] w-full grid-cols-[30%_15%_10%_15%_30%] grid-rows-[25%_25%_12%_13%_25%]">
     <!-- Left side -->
     <section class="z-10 col-start-1 col-end-5 row-start-2 row-end-5 flex flex-col gap-8 pl-20">
       <heading class="font-semibold text-primary">Sense Salon</heading>
@@ -10,11 +10,11 @@
     </section>
 
     <!-- Right side -->
-    <div class="col-start-2 col-end-3 row-start-4 row-end-6 overflow-hidden rounded-tl-[40%]">
+    <div class="col-start-2 col-end-3 row-start-4 row-end-6 overflow-hidden rounded-tl-[100px]">
       <img src="/assets/images/heroImage.webp" alt="hero image" class="h-full w-full object-cover" />
     </div>
 
-    <div class="col-start-4 col-end-6 row-start-1 row-end-6 overflow-hidden rounded-tl-[40%]">
+    <div class="col-start-4 col-end-6 row-start-1 row-end-6 overflow-hidden rounded-tl-[260px]">
       <img src="/assets/images/heroImage.webp" alt="hero image" class="h-full w-full object-cover" />
     </div>
   </div>
